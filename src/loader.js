85d305f813608aa9e2d81d3ed8f8e1bcb943ae9e
velmyk <velmyk@gmail.com>
2016-02-01 23:21:31 +0200

    Added module getter & setter

--- src/loader.js
+++ src/loader.js
@@ -2,24 +2,41 @@
 
 'use strict';
 
-function setupModuleLoader(window) {	// [MP hw1] example of singleton pattern
-	var ensure = function(obj, name, factory) {
+function setupModuleLoader(window) {
+	var ensure = function(obj, name, factory) {	// [MP hw1] example of singleton pattern
 		return obj[name] || (obj[name] = factory());
 	};
 	var angular = ensure(window, 'angular', Object);
 
-	var createModule = function(name, requires) {
-		var moduleInstance = {
-			name: name
+	ensure(angular, 'module', function() {
+		var modules = {};
+		return function(name, requires) {
+			if(requires) {
+				return createModule(name, requires, modules);
+			} else {
+				return getModule(name, modules);
+			}
 		};
+	});
 
+	var createModule = function(name, requires, modules) {
+		if (name ===  hasOwnProperty ) {
+			throw 'hasOwnProperty is not a valid module name';
+		}
+		var moduleInstance = {
+			name: name,
+			requires: requires
+		};
+		modules[name] = moduleInstance;
 		return moduleInstance;
 	};
 
-	ensure(angular, 'module', function() {
-		return function(name, requires) {
-			return createModule(name, requires);
-		};
-	});
+	var getModule = function(name, modules) {
+		if (modules.hasOwnProperty(name)) {
+			return modules[name];
+		} else {
+			throw 'Module' + name + 'is not available!';
+		}
+	};
 }
 
