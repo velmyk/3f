80a14a7e54c13f688c7139756a42e46a97e9e13c
velmyk <velmyk@gmail.com>
2016-02-02 11:54:30 +0200

    Added annotation for dependency injection

--- src/injector.spec.js
+++ src/injector.spec.js
@@ -42,4 +42,177 @@ describe('injector', function() {
 		expect(injector.get('aConstant')).toBe(42);
 	});
 
+	it('loads multiple modules', function() {
+		var module1 = angular.module('myModule', []);
+		var module2 = angular.module('myOtherModule', []);
+		module1.constant('aConstant', 42);
+		module2.constant('anotherConstant', 43);
+		var injector = createInjector(['myModule', 'myOtherModule']);
+		expect(injector.has('aConstant')).toBe(true);
+		expect(injector.has('anotherConstant')).toBe(true);
+	});
+
+	it('loads the required modules of a module', function() {
+		var module1 = angular.module('myModule', []);
+		var module2 = angular.module('myOtherModule', ['myModule']);
+		module1.constant('aConstant', 42);
+		module2.constant('anotherConstant', 43);
+		var injector = createInjector(['myOtherModule']);
+		expect(injector.has('aConstant')).toBe(true);
+		expect(injector.has('anotherConstant')).toBe(true);
+	});
+
+	it('loads the transitively required modules of a module', function() {
+		var module1 = angular.module('myModule', []);
+		var module2 = angular.module('myOtherModule', ['myModule']);
+		var module3 = angular.module('myThirdModule', ['myOtherModule']);
+		module1.constant('aConstant', 42);
+		module2.constant('anotherConstant', 43);
+		module3.constant('aThirdConstant', 44);
+		var injector = createInjector(['myThirdModule']);
+		expect(injector.has('aConstant')).toBe(true);
+		expect(injector.has('anotherConstant')).toBe(true);
+		expect(injector.has('aThirdConstant')).toBe(true);
+	});
+
+	it('loads each module only once', function() {
+		var module1 = angular.module('myModule', ['myOtherModule']);
+		var module2 = angular.module('myOtherModule', ['myModule']);
+		createInjector(['myModule']);
+	});
+
+	it('invokes an annotated function with dependency injection', function() {
+		var module = angular.module('myModule', []);
+		module.constant('a', 1);
+		module.constant('b', 2);
+		var injector = createInjector(['myModule']);
+		var fn = function(one, two) {
+			return one + two;
+		};
+		fn.$inject = ['a', 'b'];
+		expect(injector.invoke(fn)).toBe(3);
+	});
+
+	it('does not accept non-strings as injection tokens', function() {
+		var module = angular.module('myModule', []);
+		module.constant('a', 1);
+		var injector = createInjector(['myModule']);
+		var fn = function(one, two) {
+			return one + two;
+		};
+		fn.$inject = ['a', 2];
+		expect(function() {
+			injector.invoke(fn);
+		}).toThrow();
+	});
+
+	it('invokes a function with the given this context', function() {
+		var module = angular.module('myModule', []);
+		module.constant('a', 1);
+		var injector = createInjector(['myModule']);
+		var obj = {
+			two: 2,
+			fn: function(one) {
+				return one + this.two;
+			}
+		};
+		obj.fn.$inject = ['a'];
+		expect(injector.invoke(obj.fn, obj)).toBe(3);
+	});
+
+	it('overrides dependencies with locals when invoking', function() {
+		var module = angular.module('myModule', []);
+		module.constant('a', 1);
+		module.constant('b', 2);
+		var injector = createInjector(['myModule']);
+		var fn = function(one, two) {
+			return one + two;
+		};
+		fn.$inject = ['a', 'b'];
+		expect(injector.invoke(fn, undefined, {b: 3})).toBe(4);
+	});
+
+	describe('annotate', function() {
+		it('returns the $inject annotation of a function when it has one', function() {
+			var injector = createInjector([]);
+			var fn = function() { };
+			fn.$inject = ['a', 'b'];
+			expect(injector.annotate(fn)).toEqual(['a', 'b']);
+		});
+
+		it('returns the array-style annotations of a function', function() {
+			var injector = createInjector([]);
+			var fn = ['a', 'b', function() { }];
+			expect(injector.annotate(fn)).toEqual(['a', 'b']);
+		});
+
+		it('returns an empty array for a non-annotated 0-arg function', function() {
+			var injector = createInjector([]);
+			var fn = function() { };
+			expect(injector.annotate(fn)).toEqual([]);
+		});
+
+		it('returns annotations parsed from function args when not annotated', function() {
+			var injector = createInjector([]);
+			var fn = function(a, b) { };
+			expect(injector.annotate(fn)).toEqual(['a', 'b']);
+		});
+
+		it('strips comments from argument lists when parsing', function() {
+			var injector = createInjector([]);
+			var fn = function(a, /*b,*/ c) { };
+			expect(injector.annotate(fn)).toEqual(['a', 'c']);
+		});
+
+		it('strips several comments from argument lists when parsing', function() {
+			var injector = createInjector([]);
+			var fn = function(a, /*b,*/ c/*, d*/) { };
+			expect(injector.annotate(fn)).toEqual(['a', 'c']);
+		});
+
+		it('strips // comments from argument lists when parsing', function() {
+			var injector = createInjector([]);
+			var fn = function(a, //b,
+												c) { };
+			expect(injector.annotate(fn)).toEqual(['a', 'c']);
+		});
+
+		it('strips surrounding underscores from argument names when parsing', function() {
+			var injector = createInjector([]);
+			var fn = function(a, _b_, c_, _d, an_argument) { };
+			expect(injector.annotate(fn)).toEqual(['a', 'b', 'c_', '_d', 'an_argument']);
+		});
+
+		it('throws when using a non-annotated fn in strict mode', function() {
+			var injector = createInjector([], true);
+			var fn = function(a, b, c) { };
+			expect(function() { 
+				njector.annotate(fn);
+			}).toThrow();
+		});
+
+		it('invokes an array-annotated function with dependency injection', function() {
+			var module = angular.module('myModule', []);
+			module.constant('a', 1);
+			module.constant('b', 2);
+			var injector = createInjector(['myModule']);
+			var fn = ['a', 'b', function(one, two) {
+				return one + two;
+			}];
+			expect(injector.invoke(fn)).toBe(3);
+		});
+
+		it('invokes a non-annotated function with dependency injection', function() {
+			var module = angular.module('myModule', []);
+			module.constant('a', 1);
+			module.constant('b', 2);
+			var injector = createInjector(['myModule']);
+			var fn = function(a, b) {
+				return a + b;
+			};
+			expect(injector.invoke(fn)).toBe(3);
+		});
+
+	});
+
 });
